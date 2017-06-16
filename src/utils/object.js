e4fc1ff2961c20d893c24dd7490e7e8332243de5
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-16 19:00:50 +0200

    add: add object utilities

---
+++ src/utils/object.js
@@ -0,0 +1,9 @@
+function get(path, def) {
+  let object = this
+  return path
+  .split(".")
+  .filter(Boolean)
+  .every(step => ((object = object[step]) !== undefined)) ? object : def
+}
+
+export { get }
