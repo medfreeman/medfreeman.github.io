10fb819e667fcc6b5e6b396ee7b33015c8f246b5
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-16 13:19:34 +0200

    change: move `BodyRenderer` to plugin-renderer-react

--- src/presets/preset-react-app/client.js
+++ src/presets/preset-react-app/client.js
@@ -5,6 +5,6 @@ import {
   query
 } from "@phenomic/plugin-renderer-react/lib/client"
 
-import BodyRenderer from "../../renderers/BodyRenderer"
+import BodyRenderer from "../../plugins/plugin-renderer-react/BodyRenderer"
 
 export { renderApp, createApp, createContainer, query, BodyRenderer }
