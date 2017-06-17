aca23a50efc08f7818466d97d8c5454541b0bf53
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-17 17:27:58 +0200

    fix: fix static build
    Remove Home redirect from react-router
    Remove unneeded collection param

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /* eslint import/max-dependencies: 0 */
 import React from "react"
-import { Router, Route, Redirect, browserHistory } from "react-router"
+import { Router, Route, browserHistory } from "react-router"
 import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client"
 import "typeface-roboto"
 
@@ -15,12 +15,11 @@ import "./src/index.global.css"
 
 const routes = () => (
   <Router history={ browserHistory }>
-    <Redirect from="/home" to="/" />
     <Route path="/" component={ Page } />
     <Route path="/portfolio" component={ Portfolio } />
     <Route path="/blog/" component={ BlogArchive } />
     <Route path="/blog/after/:after" component={ BlogArchive } />
-    <Route path="/blog/*" component={ BlogPost } collection="posts" />
+    <Route path="/blog/*" component={ BlogPost } />
     <Route path="/*" component={ Page } />
     <Route path="*" component={ ErrorPage } />
   </Router>
