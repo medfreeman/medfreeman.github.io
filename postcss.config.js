2e884ae218ecb92289f13649a1bdd50ff2a94c86
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-20 10:09:29 +0200

    change: replace `postcss-cssnext` by contained plugins
    Adjust config
    Restore nesting in css

--- postcss.config.js
+++ postcss.config.js
@@ -5,20 +5,36 @@ module.exports = () => [
       "browserslist": "last 3 versions"
     })
   }),
-  require("postcss-cssnext")({
-    browsers: "last 3 versions",
-    features: {
-      customProperties: {
-        variables: {
-          colorPrimary: "#007acc",
-          colorText: "#555",
-          galleryElementPadding: "5px",
-          galleryElementOverlayBackgroundColor: "rgba(0,0,0,0.8)",
-          galleryElementOverlayTextColor: "#FFF"
-        },
-      },
-    },
+  require("postcss-custom-properties")({
+    variables: {
+      colorPrimary: "#007acc",
+      colorText: "#555",
+      galleryElementPadding: "5px",
+      galleryElementOverlayBackgroundColor: "rgba(0,0,0,0.8)",
+      galleryElementOverlayTextColor: "#FFF"
+    }
   }),
+  require("postcss-apply")(),
+  require("postcss-image-set-polyfill")(),
+  require("postcss-nesting")(),
+  require("postcss-custom-media")(),
+  require("postcss-media-minmax")(),
+  require("postcss-custom-selectors")(),
+  require("postcss-attribute-case-insensitive")(),
+  require("postcss-color-rebeccapurple")(),
+  require("postcss-color-hwb")(),
+  require("postcss-color-hsl")(),
+  require("postcss-color-rgb")(),
+  require("postcss-color-gray")(),
+  require("postcss-color-hex-alpha")(),
+  require("postcss-color-function")(),
+  require("postcss-font-family-system-ui")(),
+  require("postcss-font-variant")(),
+  require("postcss-initial")(),
+  require("postcss-selector-matches")(),
+  require("postcss-selector-not")(),
+  require("postcss-pseudo-class-any-link")(),
+  require("postcss-replace-overflow-wrap")(),
   require("postcss-reporter")(),
   ...(process.env.NODE_ENV !== "production") ? [
     require("postcss-browser-reporter")(),
