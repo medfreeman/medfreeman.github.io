7dd47a84aeca4084b5582f656e1ad439bb668976
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-22 17:12:21 +0200

    add: add extensible `className` prop support to `Icon`

--- src/components/Elements/Icon/index.js
+++ src/components/Elements/Icon/index.js
@@ -1,14 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
+import cx from "classnames"
 
-const Icon = props => (
-  <i className={ "mf-icons" }>
-    { props.icon }
-  </i>
-)
+const Icon = props => {
+  const { className, icon, ...otherProps } = props
+  return (
+    <i className={ cx(className, "mf-icons") } { ...otherProps }>
+      { icon }
+    </i>
+  )
+}
 
 Icon.propTypes = {
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  className: PropTypes.string
 }
 
 export default Icon
