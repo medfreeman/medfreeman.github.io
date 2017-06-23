eef317cf2aba6f7e2fba1d3e2c76c36babe49b1f
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-23 14:06:14 +0200

    change: remove style override for `buttons`
    Adapt buttons font-size and spacing
    Separate button styles
    Fix active button style

--- src/components/Elements/Link/index.js
+++ src/components/Elements/Link/index.js
@@ -7,9 +7,10 @@ import isAbsoluteUrl from "is-absolute-url"
 import Icon from "../Icon"
 
 import styles from "./index.css"
+import buttonTheme from "./button.css"
 
 const Link = (props) => {
-  const { icon, theme, to, ...otherProps } = props
+  const { icon, to, ...otherProps } = props
   if (icon) {
     if (typeof icon === "string") {
       otherProps.icon = ( <Icon icon={ icon } /> )
@@ -21,13 +22,13 @@ const Link = (props) => {
   if ( isAbsoluteUrl(to) ) {
     return (
       <a href={ to } className={ styles["link"] }>
-        <Button neutral={ false } theme={ theme || styles } { ...otherProps } />
+        <Button neutral={ false } theme={ buttonTheme } { ...otherProps } />
       </a>
     )
   } else {
     return (
       <RouterLink to={ to } className={ styles["link"] } activeClassName={ styles["link-active"] }>
-        <Button neutral={ false } theme={ theme || styles } { ...otherProps } />
+        <Button neutral={ false } theme={ buttonTheme } { ...otherProps } />
       </RouterLink>
     )
   }
