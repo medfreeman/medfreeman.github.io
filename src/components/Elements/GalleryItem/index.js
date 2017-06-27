f1efde00e9c0c03837eea43cd9158e6d8cf54ebc
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-06-27 12:34:42 +0200

    change: add 2-step touch handler with state for gallery links

--- src/components/Elements/GalleryItem/index.js
+++ src/components/Elements/GalleryItem/index.js
@@ -1,19 +1,49 @@
 import React from "react"
 import PropTypes from "prop-types"
+import cx from "classnames"
 
 import styles from "./index.css"
 
 class GalleryItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: this.props.active || false
+    }
+  }
+
+  handleTouch = (e) => {
+    if( this.state.active ) {
+      this.setState({ active: false })
+      return true
+    }
+    else {
+      this.setState({ active: true })
+      e.preventDefault()
+      return false
+    }
+  }
+
   render() {
     return (
-      <div className={ this.props.containerClass }>
-        <div className={ styles["gallery__container-inner"] }>
+      <div
+        className={ this.props.containerClass }
+      >
+        <div
+          className={
+            cx(
+              styles["gallery__container-inner"],
+              this.state.active && "hover" || null
+            )
+          }
+        >
           <div className={ styles["gallery__item"] }>
             <a
               className={ styles.link }
               href={ this.props.url }
               target="_blank"
               rel="noreferrer noopener"
+              onTouchStart={ this.handleTouch }
             >
               <img src={ this.props.image } />
               <div className={ styles["gallery__item-overlay"] }>
@@ -32,6 +62,7 @@ class GalleryItem extends React.Component {
 }
 
 GalleryItem.propTypes = {
+  active: PropTypes.bool,
   containerClass: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
