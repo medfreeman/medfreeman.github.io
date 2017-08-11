c6b7e696266fc76f904a752aa94b3759d7ef32c9
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-08-11 17:05:20 +0200

    add(app): add post date to `BlogPostHeader` component

--- src/components/Partials/BlogPostHeader/index.js
+++ src/components/Partials/BlogPostHeader/index.js
@@ -2,26 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Header from "Elements/Header";
+import P from "Elements/P";
 import Title from "Elements/Title";
 import Link from "Elements/Link";
 
-import linkTheme from "./theme.css";
+import theme from "./theme.css";
 
 const BlogPostHeader = ({ post }) => {
   const title = post.id
-    ? <Link to={`/blog/${post.id}`} theme={linkTheme}>
+    ? <Link to={`/blog/${post.id}`} theme={theme}>
         {post.title || post.id}
       </Link>
     : post.title;
 
   return (
-    <Header>
-      <Title level={2}>
+    <Header theme={theme}>
+      <Title level={2} theme={theme}>
         {title}
       </Title>
-      <p>
+      <P theme={theme}>
         {post.date}
-      </p>
+      </P>
     </Header>
   );
 };
